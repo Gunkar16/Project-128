484b610fffba95ed1724b83558c1db3fa8a8c736
Gunkar16 <76874019+Gunkar16@users.noreply.github.com>
2021-06-21 18:34:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song1 = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload(){
     song1 = loadSound("Sorry By Alan Walker.mp3");
     song2 = loadSound("Dance Monkey By Tones and I.mp3");
@@ -11,6 +17,25 @@ function setup(){
     Canvas.position(0,170);
     Video = createCapture(VIDEO);
     Video.hide();
+
+    poseNet = ml5.poseNet(Video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+    }
 }
 
 function draw(){
